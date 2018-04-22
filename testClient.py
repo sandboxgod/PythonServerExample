38a911dc07c9d35a7253898c184b511d84909e64
rosborne <rosborne255@yahoo.com>
2018-04-22 11:18:14 -0500

    Test client no longer just sends max positive int value but now increments

--- testClient.py
+++ testClient.py
@@ -2,9 +2,10 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect(('127.0.0.1', 4000))
-
+count = 0
 while True:
-    data = (2147483647).to_bytes(4,byteorder='little')
+    data = (count).to_bytes(4,byteorder='little')
     server.send(data)
+    count += 1
 
 server.close()
