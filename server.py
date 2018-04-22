b6373468d59bc6bdbf58b61b6c9a72de081ac365
rosborne <rosborne255@yahoo.com>
2018-04-22 01:35:13 -0500

    Updating to work like an ECHO server
    * todo: Will remove echo in a bit

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, select
+import socket, select, sys, queue
 
 print("Hi")
 clientId = 0 # increment for each unique client; generate id. TODO: Would be better if the client could provide us a unique id somehow
@@ -9,32 +9,46 @@ server.setblocking(0)
 server.bind(('', 4000))
 server.listen(5)
 
-clients = []
-while True:
-    Connections, wlist, xlist = select.select([server], [], [], 0.05)
+inputs = [server]
+outputs = []
+message_queues = {}
 
-    for Connection in Connections:
-        client, Informations = Connection.accept()
-        Connection.setblocking(0)
-        clients.append(client)
+while inputs:
+    readable, writable, exceptional = select.select(
+        inputs, outputs, inputs, 0)
+    for s in readable:
+        if s is server:
+            connection, client_address = s.accept()
+            connection.setblocking(0)
+            inputs.append(connection)
+            message_queues[connection] = queue.Queue()
+        else:
+            data = s.recv(4)
+            if data:
+                value = int.from_bytes(data, byteorder='little')
+                print(value)
+                message_queues[s].put(data)
+                if s not in outputs:
+                    outputs.append(s)
+            else:
+                if s in outputs:
+                    outputs.remove(s)
+                inputs.remove(s)
+                s.close()
+                del message_queues[s]
 
-    clientsList = []
-    try:
-        clientsList, wlist, xlist = select.select(clients, [], [], 0.05)
-    except select.error:
-        pass
-    else:
-        for clientInList in clientsList:
-            try:
-                data = clientInList.recv(4)
-                if (data):
-                    value = int.from_bytes(data, byteorder='little')
-                    if (clientId == 0):
-                        print(value)
-                        clientId = 1
-            except:
-                pass
+    for s in writable:
+        try:
+            next_msg = message_queues[s].get_nowait()
+        except queue.Empty:
+            outputs.remove(s)
+        else:
+            s.send(next_msg)
 
+    for s in exceptional:
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+        del message_queues[s]
 
-clientInList.close()
-server.close()
