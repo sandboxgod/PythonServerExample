e8839afee95f95e1709842f7e2b510aeda689669
rosborne <rosborne255@yahoo.com>
2018-04-23 09:32:40 -0500

    Removing the echo server stuff and adding a readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+This is just a simple Server application. I used Python 3.6.5 to build this application.
+
+To run this application from commandline:
+```bash
+python server.py
+python testClient.py
+```
+
+This application has also been dockerized. It listens on port 4000. You can build and run the application like so (if Docker is installed):
+
+```bash
+docker build -t my-python .
+docker run -it --rm --name my-running-app -p 4000:4000 my-python
+```
--- server.py
+++ server.py
@@ -1,9 +1,7 @@
-import socket, select, sys, queue
+import socket, select, sys
 
-print("Hi")
-clientId = 0 # increment for each unique client; generate id. TODO: Would be better if the client could provide us a unique id somehow
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-hostname = socket.gethostname() # Debating do we run this or not
+hostname = socket.gethostname() # Not using atm
 print("hostname: " + hostname)
 server.setblocking(0)
 server.bind(('', 4000))
@@ -11,7 +9,6 @@ server.listen(5)
 
 inputs = [server]
 outputs = []
-message_queues = {}
 
 while inputs:
     readable, writable, exceptional = select.select(
@@ -21,34 +18,25 @@ while inputs:
             connection, client_address = s.accept()
             connection.setblocking(0)
             inputs.append(connection)
-            message_queues[connection] = queue.Queue()
         else:
-            data = s.recv(4)
-            if data:
-                value = int.from_bytes(data, byteorder='little')
-                print(value)
-                message_queues[s].put(data)
-                if s not in outputs:
-                    outputs.append(s)
-            else:
-                if s in outputs:
-                    outputs.remove(s)
-                inputs.remove(s)
-                s.close()
-                del message_queues[s]
-
-    for s in writable:
-        try:
-            next_msg = message_queues[s].get_nowait()
-        except queue.Empty:
-            outputs.remove(s)
-        else:
-            s.send(next_msg)
+            try:
+                data = s.recv(4)
+                if data:
+                    value = int.from_bytes(data, byteorder='little')
+                    print(value)
+                    if s not in outputs:
+                        outputs.append(s)
+                else:
+                    if s in outputs:
+                        outputs.remove(s)
+                    inputs.remove(s)
+                    s.close()
+            except:
+                pass
 
     for s in exceptional:
         inputs.remove(s)
         if s in outputs:
             outputs.remove(s)
         s.close()
-        del message_queues[s]
 
